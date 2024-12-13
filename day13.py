2bd5bdac37e1fbea01d96ea6e7c7abcfd93b5116
Mark <mark@msc27.me.uk>
2024-12-13 13:03:21 +0000

    Python day 13: improve comment

--- day13.py
+++ day13.py
@@ -9,8 +9,8 @@ def solve(para, offset = 0):
 # ax.A + bx.B = px + offset
 # ay.A + by.B = py + offset
 # Solve the above for A and B. If the solutions are integers, we can win!
-# There can only be one solution unless both buttons move the claw
-# in the same direction by different amounts. Assume this is never the case.
+# There can only be one solution unless both buttons move the claw in the
+# same direction. Assume this is never the case (div by zero if so).
     tx = px + offset
     ty = py + offset
     A_top = tx * by - ty * bx
